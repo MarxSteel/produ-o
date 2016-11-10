676b36dafdafaa7e31c4cedfc28415ea027df693
Marx Medeiros <analista.marx@gmail.com>
2016-11-10 13:50:47 -0200

    MODAL

---
+++ P2/modal/montagemOrionAcesso.php
@@ -0,0 +1,52 @@
+<div clss="main-box-body clearfix">
+ <div class="modal fade" id="NovoOrionVI" tabindex"-1" role="dialog" aria-abeledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+   <div class="modal-content">
+    <div class="modal-header">
+     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><code>&times;</code></span></button>
+      <h4 class="modal-title">Cadastrar Equipamento: <strong><code>Orion 6</code></strong></h4>
+    </div>
+           <div class="box-body">
+                <ul class="products-list product-list-in-box">
+         <li class="item">
+          <div class="product-info">
+           <a href="javascript::;" class="product-title">Orio 6A<span class="btn btn-warning pull-right disabled" onclick="window.open('Producao2/Montagem/Orion/6A.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">NOVO <strong><code>6A</code></strong></span></a>
+            <span class="product-description"><code>BIOMETRIA + PROXIMIDADE</code></span>
+          </div>
+         </li><!-- /.item -->
+        </ul>
+        <ul class="products-list product-list-in-box">
+         <li class="item">
+          <div class="product-info">
+           <a href="javascript::;" class="product-title">Orion 6B<span class="btn bg-navy pull-right disabled" onclick="window.open('Producao2/Montagem/Orion/6B.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">NOVO <strong><code>6B</code></strong></span></a>
+            <span class="product-description"><code>BIOMETRIA + BARRAS</code></span>
+          </div>
+         </li><!-- /.item -->
+        </ul>
+        <ul class="products-list product-list-in-box">
+         <li class="item">
+          <div class="product-info">
+           <a href="javascript::;" class="product-title">Orion 6C<span class="btn bg-olive pull-right disabled" onclick="window.open('Producao2/Montagem/Orion/6C.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">NOVO <strong><code>6C</code></strong></span></a>
+            <span class="product-description"><code>BARRAS</code></span>
+          </div>
+         </li><!-- /.item -->
+        </ul>
+        <ul class="products-list product-list-in-box">
+         <li class="item">
+          <div class="product-info">
+           <a href="javascript::;" class="product-title">Orion 6D<span class="btn bg-maroon pull-right disabled" onclick="window.open('Producao2/Montagem/Orion/6D.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">NOVO <strong><code>6D</code></strong></span></a>
+            <span class="product-description"><code>PROXIMIDADE</code></span>
+          </div>
+         </li><!-- /.item -->
+        </ul>
+       </div><!-- /.box-body -->
+   
+
+
+   </div>
+  </div>
+ </div>
+</div>
+
+
+
