03d2d090f8c95fab374b51dda61e3142cd5abac6
Marx Medeiros <analista.marx@gmail.com>
2016-11-10 13:50:50 -0200

    MODAL

---
+++ P2/modal/montagemIDNOXModal.php
@@ -0,0 +1,39 @@
+<div clss="main-box-body clearfix">
+ <div class="modal fade" id="NovoIDNOX" tabindex"-1" role="dialog" aria-abeledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+   <div class="modal-content">
+    <div class="modal-header">
+     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><code>&times;</code></span></button>
+      <h4 class="modal-title">Cadastrar Equipamento: <strong><code>DIXI IDNOX (Com Impressora)</code></strong></h4>
+    </div>
+           <div class="box-body">
+        <ul class="products-list product-list-in-box">
+         <li class="item">
+           <a class="product-title">IDNOX LT-PROX - (0003800213)<span class="btn btn-warning pull-right" onclick="window.open('Cad/13/LT-PROX_E1.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR</span></a>
+           <span class="product-description">PROXIMIDADE</span>
+         </li><!-- /.item -->
+         <li class="item">
+           <a class="product-title">IDNOX SMART-PROX - (0003800214)<span class="btn btn-info pull-right" onclick="window.open('Cad/13/SMART-PROX_E1.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR</span></a>
+           <span class="product-description">PROXIMIDADE + MIFARE</span>
+         </li><!-- /.item -->
+         <li class="item">
+           <a class="product-title">IDNOX LT-BIO - (0003800215)<span class="btn btn-success pull-right" onclick="window.open('Cad/13/LT-BIO_E1.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR</span></a>
+           <span class="product-description">BIOMETRIA</span>
+         </li><!-- /.item -->
+         <li class="item">
+           <a class="product-title">IDNOX LT-BIO PROX - (0003800216)<span class="btn btn-primary pull-right" onclick="window.open('Cad/13/LT-BIOPROX_E1.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR</span></a>
+           <span class="product-description"> BIOMETRIA + PROXIMIDADE</span>
+         </li><!-- /.item -->
+         <li class="item">
+           <a class="product-title">IDNOX LT - (0003800180)<span class="btn btn-danger pull-right" onclick="window.open('Cad/13/LT_E1.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR</span></a>
+           <span class="product-description">BIOMETRIA + PROXIMIDADE + SMARTCARD</span>
+         </li><!-- /.item -->
+        </ul>
+       </div><!-- /.box-body -->
+   </div>
+  </div>
+ </div>
+</div>
+
+
+
