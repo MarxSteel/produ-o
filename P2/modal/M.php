daeaa7cf2a5fb8a0a48e0e7ac4231f2218b34a73
Marx Medeiros <analista.marx@gmail.com>
2016-11-10 13:51:19 -0200

    MODAL

---
+++ P2/modal/M.php
@@ -0,0 +1,41 @@
+<div clss="main-box-body clearfix">
+ <div class="modal fade" id="NovoOrion" tabindex"-1" role="dialog" aria-abeledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+   <div class="modal-content">
+    <div class="modal-header">
+     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><code>&times;</code></span></button>
+      <h4 class="modal-title">Cadastrar Equipamento: <strong><code>Orion</code></strong></h4>
+    </div>
+    <div class="box-body">
+     <ul class="products-list product-list-in-box">
+      <li class="item">
+       <div class="product">
+        <a href="javascript::;" class="product-title">Orion 2<span class="btn btn-warning pull-right" onclick="window.open('Producao2/Montagem/Orion/2.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR NOVO</span></a>
+       </div>
+      </li><!-- /.item -->
+     </ul>
+     <ul class="products-list product-list-in-box">
+      <li class="item">
+       <div class="product">
+        <a href="javascript::;" class="product-title">Orion 5<span class="btn bg-green pull-right" onclick="window.open('Producao2/Montagem/Orion/5.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR NOVO</span></a>
+       </div>
+      </li><!-- /.item -->
+     </ul>
+     <ul class="products-list product-list-in-box">
+      <li class="item">
+      <div class="product">
+           <a href="javascript::;" class="product-title">Orion CR<span class="btn bg-red pull-right" onclick="window.open('Producao2/Montagem/Orion/CR.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR NOVO</span></a>
+          </div>
+         </li><!-- /.item -->
+        </ul>
+       </div><!-- /.box-body -->
+   
+
+
+   </div>
+  </div>
+ </div>
+</div>
+
+
+
