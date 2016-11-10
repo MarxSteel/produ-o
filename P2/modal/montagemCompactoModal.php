b9f1a8d4ca74952b0f955fefabecf89e37511a1b
Marx Medeiros <analista.marx@gmail.com>
2016-11-10 13:51:08 -0200

    MODAL

---
+++ P2/modal/montagemCompactoModal.php
@@ -0,0 +1,40 @@
+<div clss="main-box-body clearfix">
+ <div class="modal fade" id="NovoCompacto" tabindex"-1" role="dialog" aria-abeledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+   <div class="modal-content">
+    <div class="modal-header">
+     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><code>&times;</code></span></button>
+      <h4 class="modal-title">Cadastrar Equipamento: <strong><code>COMPACTO SiS</code></strong></h4>
+    </div>
+    <div class="box-body">
+     <ul class="products-list product-list-in-box">
+      <li class="item">
+       <a class="product-title">COMPACTO S-A - (0000400384)<span class="btn btn-warning pull-right btn-xs" onclick="window.open('./Cad/04/A.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR</span></a>
+       <span class="product-description">BIOMETRIA + PROXIMIDADE + SMART CARD</span>
+      </li><!-- /.item -->
+         <li class="item">
+           <a class="product-title">COMPACTO S-B - (0000400385)<span class="btn btn-info pull-right btn-xs" onclick="window.open('./Cad/04/B.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR</span></a>
+           <span class="product-description">BIOMETRIA</span>
+         </li><!-- /.item -->
+         <li class="item">
+           <a class="product-title">COMPACTO S-C - (0000400386)<span class="btn btn-success pull-right btn-xs" onclick="window.open('./Cad/04/C.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR</span></a>
+           <span class="product-description">PROXIMIDADE + SMART CARD</span>
+         </li><!-- /.item -->
+         <li class="item">
+           <a class="product-title">COMPACTO S-D - (0000400387)<span class="btn bg-navy pull-right btn-xs" onclick="window.open('./Cad/04/D.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR</span></a>
+           <span class="product-description">BIOMETRIA + PROXIMIDADE</span>
+         </li><!-- /.item -->
+         <li class="item">
+           <a class="product-title">COMPACTO S-E - (0000400388)<span class="btn btn-danger pull-right btn-xs" onclick="window.open('./Cad/04/E.php', 'Pagina', 'STATUS=NO, TOOLBAR=NO, LOCATION=NO, DIRECTORIES=NO, RESISABLE=NO, SCROLLBARS=YES, TOP=10, LEFT=10, WIDTH=800, HEIGHT=650');">ADICIONAR</span></a>
+           <span class="product-description">BIOMETRIA +SMART CARD </span>
+         </li><!-- /.item -->
+        </ul>
+
+
+
+
+       </div><!-- /.box-body -->
+   </div>
+  </div>
+ </div>
+</div>
